ae6b8fe4ccaa69d571e48cf60de4fe875909299f
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-08-10 19:55:36 +0100

    test scroll

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
-let matrix = SmartMatrix.create(
-DigitalPin.P0,
+input.onButtonPressed(Button.A, function () {
+    matrix.scrollText(
+    "Framk",
+    24,
+    0,
+    neopixel.colors(NeoPixelColors.Blue)
+    )
+})
+let matrix: SmartMatrix.Matrix = null
+basic.showIcon(IconNames.Umbrella)
+matrix = SmartMatrix.create(
+DigitalPin.P2,
 32,
 8,
 NeoPixelMode.RGB
 )
+matrix.Brightness(32)
+matrix.clear()
+matrix.show()
 basic.forever(function () {
 	
 })
