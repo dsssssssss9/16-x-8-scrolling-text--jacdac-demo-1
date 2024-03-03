be2c71e8fb52dc1b299cfc46cb885e04afee0273
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-03-03 18:21:18 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
-/**
- * Demo to show Jacdac inputs used to control output on a 32*8 WS2812b type matrix
- * 
- * Uses following extension to output scrolling text to the display...
- * 
- * https://github.com/Shorts1999/pxt-smartmatrix
- * 
- * AT THE MOMENT THE EXTENSION ONLY WORKS WITH "COLUMN ZIG ZAG " PATTERN MATRIX
- * 
- * The extension author is working on adding other types of display layout , static text, lines/boxes etc )
- * 
- * ---------------------------------------------------------------
- * 
- * This demo uses 2 Jacdac buttons ( BUTTON2 , BUTTON3) as well as a rotary encoder with built in button ( BUTTON1)
- * 
- * Press BUTTON1 to display rotary encoder value
- * 
- * Press BUTTON2 to display a message
- * 
- * Press BUTTON3 to display a message
- * 
- * You can also press A on Micro:Bit to display a message
- */
+// Demo to show Jacdac inputs used to control output on a 32*8 WS2812b type matrix
+// 
+// Uses following extension to output scrolling text to the display...
+// 
+// https://github.com/Shorts1999/pxt-smartmatrix
+// 
+// AT THE MOMENT THE EXTENSION ONLY WORKS WITH "COLUMN ZIG ZAG " PATTERN MATRIX
+// 
+// The extension author is working on adding other types of display layout , static text, lines/boxes etc )
+// 
+// ---------------------------------------------------------------
+// 
+// This demo uses 2 Jacdac buttons ( BUTTON1 , BUTTON2)
+// 
+// Press BUTTON1 to display
+// 
+// Press BUTTON2 to display a message
+// 
+// 
+// 
+// You can also press A on Micro:Bit to display a message
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
     matrix.clear()
     matrix.show()
